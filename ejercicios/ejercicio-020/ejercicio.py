4dd7d96a0924ffa5976d45e1e7f7cfff60a75002
guosma <alekenemark@gmail.com>
2022-07-11 19:46:33 -0300

    resolviendo el ejercicio 20

--- ejercicios/ejercicio-020/ejercicio.py
+++ ejercicios/ejercicio-020/ejercicio.py
@@ -5,13 +5,29 @@ pase como parámetro)
 """
 
 
-def cambia_vocales(frase, vocal="a"):
-    pass
+from hashlib import new
+
+
+def cambia_vocales(frase:str, vocal="a") -> str:
+    
+    vocales = ['a','e','i','o','u']
+    
+    if vocal in vocales:
+        vocales.remove(vocal)
+
+    new_frase = frase
+    
+    for letra in frase:
+
+        if letra in vocales:
+            new_frase = new_frase.replace(letra,vocal)
+    
+    return new_frase
 
 
 # NO BORRAR LAS LINEAS QUE SIGUEN
 # Una vez terminada la funcion ejecutar este archivo
-# Si se ve la leyenda 'Ejercicio terminado OK' esta listo, crear un PR
+# Si se ve la leyenda 'Ejercicio terminado OK' esta listo, crear un PR 
 
 assert cambia_vocales("hola") == "hala"
 assert cambia_vocales("Juan Carlos") == "Jaan Carlas"
